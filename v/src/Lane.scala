fdb6b83246ad2c688f049731c7013a9533a94eb2
chester <liqinjun1994@gmial.com>
2022-07-14 17:25:30 +0800

    lane control & decode.(wip)

--- v/src/Lane.scala
+++ v/src/Lane.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.internal.sourceinfo.SourceInfo
 import chisel3.util._
 
-class LaneReq(param: VectorParameters) extends Bundle {
+class LaneReq(param: LaneParameters) extends Bundle {
   val index: UInt = UInt(param.idBits.W)
   val uop: UInt = UInt(4.W)
   val w: Bool = Bool()
@@ -17,7 +17,7 @@ class LaneReq(param: VectorParameters) extends Bundle {
   val maskDestination: Bool = Bool()
 }
 
-class LaneResp(param: VectorParameters) extends Bundle {
+class LaneResp(param: LaneParameters) extends Bundle {
   val mask: UInt = UInt(4.W)
 }
 
@@ -44,15 +44,16 @@ class LaneDecodeResult extends Bundle {
   val subUop: UInt = UInt(3.W)
 }
 
-class LaneSrcResult(param: VectorParameters) extends Bundle {
+class LaneSrcResult(param: LaneParameters) extends Bundle {
   val src0: UInt = UInt(param.ELEN.W)
   val src1: UInt = UInt(param.ELEN.W)
   val src2: UInt = UInt(param.ELEN.W)
   val src3: UInt = UInt(2.W)
+  val mask: UInt = UInt(param.ELEN.W)
   val desMask: UInt = UInt(param.ELEN.W)
 }
 
-class Lane(param: VectorParameters) extends Module {
+class Lane(param: LaneParameters) extends Module {
   val req: DecoupledIO[LaneReq] = IO(Flipped(Decoupled(new LaneReq(param))))
   val resp: ValidIO[LaneResp] = IO(Valid(new LaneResp(param)))
 
@@ -86,6 +87,7 @@ class Lane(param: VectorParameters) extends Module {
     res.src1 := SubtractionOperand1
     res.src2 := SubtractionOperand2
     res.src3 := negativeCompensation
+    res.mask := ((1 << sew) - 1).U(param.ELEN.W)
     // todo handle w n
     res.desMask := ((1 << sew) - 1).U(param.ELEN.W)
     res
@@ -94,7 +96,47 @@ class Lane(param: VectorParameters) extends Module {
   val srcSelect: LaneSrcResult = Mux1H(UIntToOH(req.bits.sew), LaneSrcVec)
 
   // alu
-  val logicUnit: LaneLogic = Module(new LaneLogic(param.laneLogicParam))
+  val logicUnit: LaneLogic = Module(new LaneLogic(param.datePathParam))
+  val adder: LaneAdder = Module(new LaneAdder(param.datePathParam))
+  val shifter: LaneShifter = Module(new LaneShifter(param.shifterParameter))
+  val mul: LaneMul = Module(new LaneMul(param.mulParam))
+  val div: LaneDiv = Module(new LaneDiv(param.datePathParam))
+  val popCount: LanePopCount = Module(new LanePopCount(param.lanePopCountParameter))
+  val ffo: LaneFFO = Module(new LaneFFO(param.datePathParam))
+  val getID: LaneIndexCalculator = Module(new LaneIndexCalculator(param.indexParam))
+  val dp: LaneDataProcessing = Module(new LaneDataProcessing(param.datePathParam))
+
+  val resultVec: Vec[UInt] = Wire(Vec(8, UInt(param.mulRespWidth.W)))
+  val carryRes: UInt = Wire(UInt(param.mulRespWidth.W))
+
+  // logicUnit connect
+  val logicInput: LaneSrcResult = Mux(decodeRes.logic, srcSelect, 0.U.asTypeOf(srcSelect))
+  logicUnit.src :=  VecInit(Seq(logicInput.src0, logicInput.src1))
+  logicUnit.opcode :=  decodeRes.subUop
+  resultVec.head := Mux(decodeRes.logic, logicUnit.resp, 0.U)
+
+  // adder connect
+  val addInput: LaneSrcResult = Mux(decodeRes.arithmetic, srcSelect, 0.U.asTypeOf(srcSelect))
+  adder.src :=  VecInit(Seq(addInput.src0, addInput.src1, addInput.src3))
+  resultVec(1) := Mux(decodeRes.logic, adder.resp, 0.U)
+
+  // shifter connect
+  val shiftInput: LaneSrcResult = Mux(decodeRes.shift, srcSelect, 0.U.asTypeOf(srcSelect))
+  shifter.src := shiftInput.src0
+  shifter.sign := decodeRes.subUop(0)
+  shifter.direction := decodeRes.subUop(1)
+  shifter.shifterSize := shiftInput.src2
+  shifter.mask := shiftInput.mask
+  resultVec(2) := Mux(decodeRes.logic, shifter.resp, 0.U)
+
+  // mul connect
+  val mulInput: LaneSrcResult = Mux(decodeRes.mul, srcSelect, 0.U.asTypeOf(srcSelect))
+  mul.src :=  VecInit(Seq(addInput.src0, addInput.src1, addInput.src2))
+  resultVec(3) := Mux(decodeRes.logic, mul.resp.head, 0.U)
+  carryRes := mul.resp.last
+
+  // div
+  val divInput: LaneSrcResult = Mux(decodeRes.div, srcSelect, 0.U.asTypeOf(srcSelect))
   resp <> DontCare
   req <> DontCare
 }
